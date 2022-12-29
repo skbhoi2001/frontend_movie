3c98e50b509968a580a3b6b6c7e0a0ac102c2a7e
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-12-29 22:47:57 +0530

    initial setup done

--- components/common/apiOperation.js
+++ components/common/apiOperation.js
@@ -1,6 +1,7 @@
-import { postApi } from './apiPromise';
+import { deleteApi, getApi, postApi } from './apiPromise';
 
-let baseUserUrl = `http://localhost:8000/api/user`;
+let baseUserUrl = `http://localhost:8000/api`;
+let token = `Generate token`;
 
 export async function api_createUser({ name, email, password }) {
   let url = `${baseUserUrl}/create`;
@@ -16,7 +17,7 @@ export async function api_createUser({ name, email, password }) {
 }
 
 export async function api_verifyUser({ userId, OTP }) {
-  let url = `${baseUserUrl}/verify-email`;
+  let url = `${baseUserUrl}/user/verify-email`;
   let headers = {
     'Content-Type': 'application/json',
   };
@@ -28,7 +29,7 @@ export async function api_verifyUser({ userId, OTP }) {
 }
 
 export async function api_resendEmailVerifification({ userId }) {
-  let url = `${baseUserUrl}/resend-email-verification-token`;
+  let url = `${baseUserUrl}/user/resend-email-verification-token`;
   let headers = {
     'Content-Type': 'application/json',
   };
@@ -39,7 +40,7 @@ export async function api_resendEmailVerifification({ userId }) {
 }
 
 export async function api_signIn({ email, password }) {
-  let url = `${baseUserUrl}/sign-in`;
+  let url = `${baseUserUrl}/user/sign-in`;
   let headers = {
     'Content-Type': 'application/json',
   };
@@ -51,7 +52,7 @@ export async function api_signIn({ email, password }) {
 }
 
 export async function api_forgotPassword({ email }) {
-  let url = `${baseUserUrl}/forget-password`;
+  let url = `${baseUserUrl}/user/forget-password`;
   let headers = {
     'Content-Type': 'application/json',
   };
@@ -61,4 +62,68 @@ export async function api_forgotPassword({ email }) {
   return postApi(url, body, headers);
 }
 
-export async function api_resetPassword({ token, userId, newPassword }) {}
+export async function api_resetPassword({ token, userId, newPassword }) {
+  let url = `${baseUserUrl}/user/reset-password`;
+  let headers = {
+    'Content-Type': 'application/json',
+  };
+  let body = {
+    token: token,
+    userId: userId,
+    newPassword: newPassword,
+  };
+}
+
+export async function api_createActor({ formData }) {
+  let url = `${baseUserUrl}/actor/create`;
+  let headers = {
+    authorization: 'Bearer ' + token,
+    'content-type': 'multipart/form-data',
+  };
+  let form = formData;
+  return postApi(url, form, headers);
+}
+
+export async function api_updateActor({ formData, id }) {
+  let url = `${baseUserUrl}/actor/update/${id}`;
+  let headers = {
+    authorization: 'Bearer ' + token,
+    'content-type': 'multipart/form-data',
+  };
+  let form = formData;
+  return postApi(url, form, headers);
+}
+
+export async function api_searchActorAdmin({ name }) {
+  let url = `${baseUserUrl}/actor/search?name=${name}`;
+  let headers = {
+    authorization: 'Bearer ' + token,
+  };
+  return getApi(url, headers);
+}
+
+export async function api_searchActor({ name }) {
+  let url = `${baseUserUrl}/actor/search?name=${name}`;
+  let headers = {};
+  return getApi(url, headers);
+}
+
+export async function api_deleteActor({ id }) {
+  let url = `${baseUserUrl}/actor/${id}`;
+  let headers = {
+    authorization: 'Bearer ' + token,
+  };
+  return deleteApi(url, '', headers);
+}
+
+export async function api_latestActor() {
+  let url = `${baseUserUrl}/actor/latest-uploads`;
+  let headers = {};
+  return getApi(url, headers);
+}
+
+export async function api_getActors() {
+  let url = `${baseUserUrl}/actor/actorsUser`;
+  let headers = {};
+  return getApi(url, headers);
+}
