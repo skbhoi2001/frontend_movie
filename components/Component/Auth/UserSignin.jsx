220dbc5427ae91ab957c620ff17d342b3d379686
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2023-01-04 22:51:44 +0530

    signup

--- components/Component/Auth/UserSignin.jsx
+++ components/Component/Auth/UserSignin.jsx
@@ -2,12 +2,27 @@ import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { api_signIn } from '../../common/apiOperation';
+import Styles from '../../../styles/auth.module.css';
+import AuthCommon from './AuthCommon';
+
+import { TextField } from '@mui/material';
 
 const UserSignin = () => {
   const [cookieData, setCookieData] = useCookies();
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signInModel, setSignInModel] = useState(true);
+  const [signUpModel, setSignUpModel] = useState(false);
+
+  const handleSignup = () => {
+    setSignInModel(false);
+    setSignUpModel(true);
+  };
+  const handleSignin = () => {
+    setSignInModel(true);
+    setSignUpModel(false);
+  };
 
   const handleLogin = async () => {
     const signin = await api_signIn({ email, password });
@@ -24,11 +39,77 @@ const UserSignin = () => {
   };
 
   return (
-    <div>
-      <input type='text' onChange={(e) => setEmail(e.target.value)} />
-      <input type='text' onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={() => handleLogin()}>Signin</button>
-      <a href='/auth/forgotPassword'>Forgot Password</a>
+    <div className={Styles.signupContainer}>
+      <div className={Styles.sidebarContainer}>
+        <AuthCommon />
+      </div>
+      {signInModel && (
+        <div className={Styles.authContainer}>
+          <div className={`fontType0 ${Styles.userSigninText}`}>
+            User Sign-In
+          </div>
+          <TextField
+            label='Email'
+            id='outlined-basic'
+            variant='outlined'
+            onChange={(e) => setEmail(e.target.value)}
+            className={Styles.textContainer}
+          />
+          <TextField
+            id='outlined-basic'
+            label='Password'
+            variant='outlined'
+            onChange={(e) => setPassword(e.target.value)}
+            className={Styles.textContainer}
+          />
+          <button
+            className={`globalButton ${Styles.authBtn}`}
+            onClick={() => handleLogin()}
+          >
+            Signin
+          </button>
+          <div className={Styles.forgot_create_acc}>
+            <div className='fontType1'>Forgot Password</div>
+            <div className='fontType2' onClick={() => handleSignup()}>
+              New User - <span className='fontType3'>Create Account</span>
+            </div>
+          </div>
+        </div>
+      )}
+      {signUpModel && (
+        <div className={Styles.authContainer}>
+          <div className={`fontType0 ${Styles.userSigninText}`}>
+            User Sign-In
+          </div>
+          <TextField
+            label='Email'
+            id='outlined-basic'
+            variant='outlined'
+            onChange={(e) => setEmail(e.target.value)}
+            className={Styles.textContainer}
+          />
+          <TextField
+            id='outlined-basic'
+            label='Password'
+            variant='outlined'
+            onChange={(e) => setPassword(e.target.value)}
+            className={Styles.textContainer}
+          />
+          <button
+            className={`globalButton ${Styles.authBtn}`}
+            onClick={() => handleLogin()}
+          >
+            Signin
+          </button>
+          <div className={Styles.forgot_create_acc}>
+            <div className='fontType1'>Forgot Password</div>
+            <div className='fontType2'>
+              New User - <span className='fontType3'>Create Account</span>
+            </div>
+          </div>
+          <a href='/auth/forgotPassword'></a>
+        </div>
+      )}
     </div>
   );
 };
