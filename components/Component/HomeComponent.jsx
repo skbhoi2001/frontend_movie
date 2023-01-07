530431991551e1a2bc76155d6366312932c13e99
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2023-01-07 16:04:36 +0530

    viewer added

--- components/Component/HomeComponent.jsx
+++ components/Component/HomeComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-const HomeComponent = () => {
+const HomeComponent = ({ setDiffTheme }) => {
   return (
     <div>
-      <Navbar />
+      <Navbar setDiffTheme={setDiffTheme} />
     </div>
   );
 };
--- components/Component/Navbar.jsx
+++ components/Component/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from '../../styles/Navbar.module.css';
 import { useCookies } from 'react-cookie';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 
-const Navbar = () => {
+const Navbar = ({ setDiffTheme }) => {
   const [cookieData, setCookieData, removeCookie] = useCookies();
+  const [themeIcon, setThemeIcon] = useState(false);
   const router = useRouter();
   const handleLogout = async () => {
     await removeCookie('userstatus', { path: '/' });
@@ -16,19 +19,12 @@ const Navbar = () => {
     await removeCookie('userid', { path: '/' });
     router.push('/');
   };
+  const themeUpdate = () => {
+    setDiffTheme();
+    setThemeIcon(!themeIcon);
+  };
   return (
     <div className={Styles.navContainer}>
-      {/* <div>Movie Database</div>
-      <div>
-        {cookieData?.usertoken && cookieData?.userstatus ? (
-          <div>
-            <div>Welcome</div>
-            <button onClick={() => handleLogout()}>Logout</button>
-          </div>
-        ) : (
-          <a href='/auth/signup'>Signup</a>
-        )}
-      </div> */}
       <div>REVIEW_ME</div>
       <div>
         <input className={Styles.searchInput} type='text' />
@@ -43,6 +39,9 @@ const Navbar = () => {
           <Link href='/auth'>Account</Link>
         )}
       </div>
+      <button className={Styles.changeColor} onClick={() => themeUpdate()}>
+        {!themeIcon ? <DarkModeIcon /> : <Brightness4Icon />}
+      </button>
     </div>
   );
 };
