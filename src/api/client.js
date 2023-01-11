deb16c540863dd7aca531a9fe26cf5f299f1e7cd
Soumyak <59327067+skbhoi2001@users.noreply.github.com>
2023-01-11 23:00:31 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://authentication-autherization.vercel.app/api",
 });
 
 export default client;
